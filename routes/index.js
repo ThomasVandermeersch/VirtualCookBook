34dbe721d5663b8c2d79d4561e3c593a879c6ee5
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2020-11-13 22:25:32 +0100

    if errors in filling form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 router.get('/', (req, res) => {
     //   res.send("<h1>It works!</h1>");
@@ -10,7 +10,28 @@ router.get('/', (req, res) => {
 module.exports = router;
 
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.render('form', { title: 'Registration form' });
-});
+router.post('/',
+    [
+        check('name') //to verify 'name' is not EMPTY
+            .isLength({ min: 1 })
+            .withMessage('Please enter a name'),
+        check('email') //to verify 'email' is an email
+            .isEmail()
+            .withMessage('Please enter an email'),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+            res.send('Thank you for your registration!');
+            console.log(req.body);
+        } else {
+            res.render('form', {
+                title: 'Registration form',
+                errors: errors.array(),
+                data: req.body,
+            });
+            
+        }
+
+    });
