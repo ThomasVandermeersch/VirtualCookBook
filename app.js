4443547d49a6337835826092d6abc6c441eccdee
ThomasVECAM <17030@ecam.be>
2020-11-13 22:41:56 +0100

    Product and Reciepe to database

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+//Creation of a server
 const express = require("express")
 app = express()
 
+const addProduct = require("./addProduct.js")
+const addReciepe = require("./addReciepe")
+
 app.get("/",function(req, res){
     res.end("<h1> Hello World </h1>")
+})
+
+app.post("/addProduct",function(req,res){
+    res.end("<h1> Document inserted </h1>");
+    addProduct(req.query)
+})
+
+app.post("/addReciepe", (req,res)=>{
+    res.end("<h1> Document inserted </h1>");
+    addReciepe(req.query)
 })
 
 app.listen(8000)
