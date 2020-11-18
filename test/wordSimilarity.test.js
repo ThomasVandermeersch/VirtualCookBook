d035202709cf4a5ac2233621f3287936d2c3b285
ThomasVECAM <17030@ecam.be>
2020-11-19 00:03:52 +0100

    similary word capital letters write test fail

--- test/wordSimilarity.test.js
+++ test/wordSimilarity.test.js
@@ -10,10 +10,27 @@ objectArrayResponse1 = [object2,object3]
 
 
 describe('Word similarity test',()=>{
-    it('should return ',()=>{
+    it('should return two list of same objects ',()=>{
         assert.equal(
             JSON.stringify(wordSimilarity(objectArray,"haricots","name",0.7)),
             JSON.stringify(objectArrayResponse1)
         )
     })
 });
+
+
+capitalObject1 = {"name":"BoNjOuR","price":20}
+capitalObject2 = {"name":"Bonjourr","prince":20}
+capitalObject3 = {"name":"Bonjourrre","price":20}
+
+objectArray2 = [capitalObject1,capitalObject2,capitalObject3]
+objectArrayResponse2 = [capitalObject1,capitalObject2,capitalObject3]
+
+describe('Word similarity capital letters issue',()=>{
+    it('should return two list of same objects ',()=>{
+        assert.equal(
+            JSON.stringify(wordSimilarity(objectArray2,"bonjour","name",0.2)),
+            JSON.stringify(objectArrayResponse2)
+        )
+    })
+})
