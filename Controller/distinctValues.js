eae806fa32352c374a55f4ba78c7f3cb4695486c
ThomasVECAM <17030@ecam.be>
2020-11-20 21:44:33 +0100

    distinct values ok -- test pass

--- Controller/distinctValues.js
+++ Controller/distinctValues.js
@@ -1,4 +1,12 @@
+const { response } = require("express")
+
 //Recieves an array of objects and returns an array
-module.exports = function distinctValues(aray,field){
-    return []
+module.exports = function distinctValues(array,field){
+    var responseArray = []
+    for( var i=0 ; array.length > i ; i++ ){
+        if(responseArray.includes(array[i][field]) == false){
+            responseArray.push(array[i][field])
+        }
+    }
+    return responseArray
 }
