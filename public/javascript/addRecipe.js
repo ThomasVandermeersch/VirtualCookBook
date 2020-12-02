3fddf0371a98b4f1a741c499ce0703ef510ce61f
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2020-12-02 11:45:03 +0100

    [team work] add step recipe

---
+++ public/javascript/addRecipe.js
@@ -0,0 +1,12 @@
+step = 3;
+button = document.getElementById("NewStep");
+formulaire = document.getElementById("formulaire");
+button.addEventListener("click", function(){
+    step +=1
+    HTML = "<label for=step"+step+" > Step " +step+ " : </label>"
+    HTML2 = "<input type='text' name=step"+step+" class='form-control' required=''</input>"
+    var container = document.createElement("div")
+    container.innerHTML = HTML + HTML2
+    formulaire.appendChild(container)
+});
+
